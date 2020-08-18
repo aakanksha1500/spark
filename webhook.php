38bb13c9f0cb38cd98de60236da11c2c6e049470
Aakanksha <aakankshadeshpande527@gmail.com>
2020-08-18 14:54:20 +0530

    connect

--- webhook.php
+++ webhook.php
@@ -5,5 +5,13 @@ $data=$_POST;
 $payment_id = $data['payment_id'];
 $payment_request_id = $data['payment_request_id'];
 
-mysql_query("INSERT INTO webhook (imojo_id, payment_id, status) VALUES ($payment_id, $payment_request_id, '')");
+
+
+$stmt = $pdo->prepare('INSERT INTO webhook
+			(imojo_id, payment_id, status) VALUES ( :mk, :yr, :mi)');
+		$stmt->execute(array(
+			':mk' => $_POST['imojo_id'],
+			':yr' => $_POST['payment_id'],
+			':mi' => $_POST['status'])
+		);
 ?>
