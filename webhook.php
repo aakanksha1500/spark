2b20612188dad66b0b50f20301b1903d5fce32cd
Aakanksha <aakankshadeshpande527@gmail.com>
2020-08-18 14:47:40 +0530

    try

---
+++ webhook.php
@@ -0,0 +1,9 @@
+<?php
+require "connect.php";
+
+$data=$_POST;
+$payment_id = $data['payment_id'];
+$payment_request_id = $data['payment_request_id'];
+
+mysql_query("INSERT INTO webhook (imojo_id, payment_id, status) VALUES ($payment_id, $payment_request_id, '')");
+?>
